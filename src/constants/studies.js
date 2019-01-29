2f72d02868520ac5101f2bed22728e1db3289998
Esteban Aragon <estebangular.aragon@gmail.com>
2019-01-29 13:21:35 -0500

    Cambios

--- src/constants/studies.js
+++ src/constants/studies.js
@@ -3,8 +3,8 @@ export const STUDIES = [
       id: 0,
       name: 'Platzi',
       image: 'https://www.siliconweek.com/wp-content/uploads/2015/12/tOQVZ4wG1.png',
-      info: "I study at Platzi, I have learned about web programming and mobile programming, focused in languages like Javascript, Python and Kotlin. Right now I'm studying all about develop of mobiles native apps to iOS. ",
-      infoSpanish: "Estudio en Platzi, he aprendido sobre programación web y programación móvil, enfocado en lenguajes como Javascript, Python y Kotlin. En este momento estoy estudiando todo sobre desarrollo de aplicaciones móviles nativas para iOS.",
+      info: "I have learned all about web programming in frontend and backend and mobile programming, focused on languages such as Javascript, Python and Kotlin. At this moment I am learning more about Python and Django.",
+      infoSpanish: "He aprendido todo sobre programación web tanto en frontend como en backend y programación móvil, enfocado en lenguajes como Javascript, Python y Kotlin. En este momento estoy aprendiendo más sobre Python y Django.",
       certificates: [
          'https://firebasestorage.googleapis.com/v0/b/website-esteban.appspot.com/o/Captura%20de%20pantalla%202019-01-14%20a%20la(s)%205.22.39%20p.%20m..png?alt=media&token=cb1aa236-a4d1-4283-be4b-46ee75dea243',
          'https://firebasestorage.googleapis.com/v0/b/website-esteban.appspot.com/o/Captura%20de%20pantalla%202019-01-14%20a%20la(s)%205.22.13%20p.%20m..png?alt=media&token=f12bbc37-5a6f-459f-82f3-7033ecc829fe',
@@ -14,6 +14,9 @@ export const STUDIES = [
          'https://firebasestorage.googleapis.com/v0/b/website-esteban.appspot.com/o/Captura%20de%20pantalla%202019-01-14%20a%20la(s)%205.23.21%20p.%20m..png?alt=media&token=98968372-b1c0-4497-b55a-f5b79e9c7989',
          'https://firebasestorage.googleapis.com/v0/b/website-esteban.appspot.com/o/Captura%20de%20pantalla%202019-01-14%20a%20la(s)%205.23.36%20p.%20m..png?alt=media&token=c46909da-2ba8-445a-83f8-37c98c8257cd',
          'https://firebasestorage.googleapis.com/v0/b/website-esteban.appspot.com/o/Captura%20de%20pantalla%202019-01-14%20a%20la(s)%205.22.54%20p.%20m..png?alt=media&token=eb440fae-069d-4eef-b36d-3e613d812fa6',
+         'https://firebasestorage.googleapis.com/v0/b/website-esteban.appspot.com/o/Captura%20de%20pantalla%202019-01-29%20a%20la(s)%2012.12.32%20p.%20m..png?alt=media&token=bca2079c-42ac-4605-b4ac-f4845d6ee60f',
+         'https://firebasestorage.googleapis.com/v0/b/website-esteban.appspot.com/o/Captura%20de%20pantalla%202019-01-29%20a%20la(s)%2012.14.32%20p.%20m..png?alt=media&token=d25cb185-9864-4d2e-a6a4-1dc03261e6e9',
+         'https://firebasestorage.googleapis.com/v0/b/website-esteban.appspot.com/o/Captura%20de%20pantalla%202019-01-29%20a%20la(s)%2012.15.17%20p.%20m..png?alt=media&token=0cea2b7b-19a9-4525-aa0c-46d66f5b415d'
       ],
       margin: 0
    },
